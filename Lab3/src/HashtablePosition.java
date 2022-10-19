9d4e057d15f13c771d1490114fe1792748e99aa5
IuliaPapureanu <papureanu.iulia@gmail.com>
2022-10-19 13:27:06 +0300

    symbol table started

---
+++ Lab3/src/HashtablePosition.java
@@ -0,0 +1,9 @@
+public class HashtablePosition {
+    int hashPosition;
+    int slot;
+
+    public HashtablePosition(int hashPosition, int slot) {
+        this.hashPosition = hashPosition;
+        this.slot = slot;
+    }
+}
